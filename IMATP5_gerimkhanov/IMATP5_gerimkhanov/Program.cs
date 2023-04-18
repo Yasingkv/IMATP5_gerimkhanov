f32229fba16ffa3fa6426ce9325ebb28667726de
Gerimkhanov Yasin <200460@CCM-Asty-Moulin.local>
2023-04-18 10:11:00 +0200

    inteero

---
+++ IMATP5_gerimkhanov/IMATP5_gerimkhanov/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace IMATP5_gerimkhanov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choix;
+            int nb1;
+            int n;
+            int nb;
+            nb1 = 0;
+            nb = 0;
+            int[,] m;
+            string stringTab;
+
+            traitementOutils mesOutils = new traitementOutils();
+            do
+            {
+                mesOutils.LireReel("voici le tableau de pascal  \n   1 = tableau de pascal \n  ", out choix);
+
+                if (choix == 1)
+                {
+                    mesOutils.LireReel("le coefficient :", out n);
+                    mesOutils.matrice(nb, nb1, n, out m);
+                    mesOutils.String_Tableau(m, out stringTab);
+
+
+                }
+                
+            } while (true);
+        }
+    }
+}
---
+++ IMATP5_gerimkhanov/IMATP5_gerimkhanov/traitementOutils.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMATP5_gerimkhanov
+{
+    class traitementOutils
+    {
+        public void LireReel(string question, out int n)
+        {
+            string nUser;
+            Console.Write(question);
+            nUser = Console.ReadLine();
+            while (!int.TryParse(nUser, out n))
+            {
+                Console.WriteLine("Attention ! vous devez taper un nombre réel !");
+                nUser = Console.ReadLine();
+            }
+        }
+        public void matrice(int nb,int nb1,int n, out int[,] m )
+        {
+            m = new int[n, n];
+            for (int i = 0; i < n +1; i++)
+            {
+                nb1 = i;
+                for (int j = 0; j < n +1; j++)
+                {
+                    nb = i + nb1;
+                }
+            }
+        }
+        
+        public void String_Tableau(int[,] m, out string stringTab)
+        {
+
+            stringTab = "";
+            for (int ligne = 0; n < m.GetLength(0); n++)
+            {
+                for (int n = 0; n < m.GetLength(1); n++)
+                {
+                    stringTab += m[n, n] + "  |  ";
+                }
+                stringTab += "\n";
+            }
+
+
+        }
+        public void coefficients()
+        {
+
+            for (int i = 0; i < m.GetLength(0); i++)
+            {
+                nb2 = nb + a +^n
+                nb2 = nb + "a" + ^"n"
+                if (n == 0)
+                {
+                    nb2 = nb + a + b+^n
+                    nb2 = nb + a + b + ^n -1
+                }
+                else
+                {
+                    ""
+                }
+            }
+        }
+    }
+}
